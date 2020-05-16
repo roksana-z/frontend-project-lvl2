6c9e1dabf8552ee69d434b8e464cb5dc3508882d
roksana-z <elleygj@gmail.com>
2020-05-16 20:32:08 +0300

    json files comparison

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
-import comander from 'commander';
-
 const gendiff = require('commander');
-
+// const fs = require('fs');
+import jsonCompare  from './bin/jsonCompare.js'
 
 gendiff
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    
-gendiff.parse(process.argv);
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .arguments('<file1Path> <file2Path>')
+  .option('-f, --format [type]', 'output format')
+  .action((file1Path, file2Path) => {
+    const file1 = file1Path;
+    const file2 = file2Path;
+    jsonCompare(file1, file2)
+  })
 
+export default gendiff;
